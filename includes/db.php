df2c6640f162721504497cccb8bc583496fb9e12
LeiyangZuo <LeiyangZuo@gmail.com>
2018-03-12 17:08:39 +1100

    Add Structure

---
+++ includes/db.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 12/3/18
+ * Time: 16:33
+ */
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "root";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value) {
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if(!$connection) {
+    die("database connect failed" . mysqli_error($connection));
+} else {
+    echo "connected";
+}
+
+
+//$connection = mysqli_connect('localhost','root','root','cms');
+//if(!$connection) {
+//    die("database connect failed" . mysqli_error($connection));
+//} else {
+//    echo "connected";
+//}
